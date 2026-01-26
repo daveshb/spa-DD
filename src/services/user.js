593f6f36ed0cdc1cec1b22259e4402e4467202c1
David Henao <daves.hb@hotmail.com>
2026-01-26 09:08:00 -0500

    add todolist

--- src/services/user.js
+++ src/services/user.js
@@ -6,6 +6,17 @@ export const getUsers = async () => {
     return response
 }
 
+// export const getUsers = async () => {
+//   try {
+//     const response = await axios.get(API_URL);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching users:', error);
+//     throw error;
+//   }
+// };
+
+
 
 export const getUserById = async (id) => {
 
