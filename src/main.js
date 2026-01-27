ec8e2329ce8eb66823a57b2d9fc85b82090aab98
David Henao <daves.hb@hotmail.com>
2026-01-27 07:44:44 -0500

    add post user

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { getUserById, getUsers } from "./services/user";
+import { createUser, getUserById, getUsers } from "./services/user";
 import {
   getTodos,
   createTodo,
@@ -78,7 +78,8 @@ async function navigate(pathname) {
 async function initLogin() {
 
   const result = await getUsers()
-  const user3 = await getUserById(3)
+  const user3 = await getUserById(1)
+
 
   
   console.log(result)
@@ -150,6 +151,66 @@ function initAbout() {
 }
 
 function initContact() {
+
+  const inputname = document.getElementById("inputName");
+  const inputemail = document.getElementById("inputEmail");
+  const inputage = document.getElementById("inputAge");
+  const inputcity = document.getElementById("inputCity");
+  const btnCreateUser = document.getElementById("btnCreateUser");
+
+  let name;
+  let email;
+  let age;
+  let city;
+
+  inputname.addEventListener("input",(e)=>{
+    name = e.target.value
+  })
+  inputemail.addEventListener("input",(e)=>{
+    email = e.target.value
+  })
+  inputage.addEventListener("input",(e)=>{
+    age = e.target.value
+  })
+  inputcity.addEventListener("input",(e)=>{
+    city = e.target.value
+  })
+
+  btnCreateUser.addEventListener("click",async()=>{
+
+    const user = {
+      name:  name,
+      email: email,
+      age: age,
+      city: city
+    }
+    
+    const response = await createUser(user);
+    console.log(response)
+
+    if (response){
+       console.log( "Se aguardo el usuario")
+      }
+    
+    // createUser(user).then((resp)=>{
+    //   console.log(resp)
+    //   if (resp){
+    //    console.log( "Se aguardo el usuario")
+    //   }
+    // })
+
+
+
+  })
+
+
+
+
+
+
+
+
+
   // Lógica específica para la vista contact
   console.log("Vista Contact cargada");
 }
--- src/services/user.js
+++ src/services/user.js
@@ -1,10 +1,33 @@
 export const getUsers = async () => {
+  try {
+    const data = await fetch("http://localhost:3000/usuarios");
+    const response = await data.json();
+    return response;
+  } catch (err) {
+    console.log("Internal error", err);
+  }
+};
 
-    const data = await fetch("http://localhost:3000/usuarios")
-    const response = await data.json()
+export const createUser = async (user) => {
+  try {
+    const data = await fetch("http://localhost:3000/usuarios", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user)
+    });
+    const response = await data.json();
+    return response;
+  } catch (err) {
+    console.log("Internal error", err);
+  }
+};
 
-    return response
-}
+// fetch(url, opciones)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error('Error:', error));
 
 // export const getUsers = async () => {
 //   try {
@@ -16,12 +39,16 @@ export const getUsers = async () => {
 //   }
 // };
 
-
-
 export const getUserById = async (id) => {
+  try {
+    if (!id) {
+      throw new Error("El id es obligatorio");
+    }
 
-    const data = await fetch(`http://localhost:3000/usuarios/${id}`)
-    const response = await data.json()
-
-    return response
-}
+    const data = await fetch(`http://localhost:3000/usuarios/${id}`);
+    const response = await data.json();
+    return response;
+  } catch (err) {
+    console.error("Internal error", err);
+  }
+};
